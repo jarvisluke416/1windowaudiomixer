dd0f28e304c1b8d8d160821c22095a0ef989e89f
Luke Jarvis <152292720+jarvisluke416@users.noreply.github.com>
2026-08-16 11:35:00 -0400

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -175,45 +175,104 @@ struct DrumEvent
 };
 
 // ============================================================
-// NOTE FREQUENCY
+// NOTE FREQUENCY - FULL 88-KEY PIANO
 // ============================================================
+//
+// Standard 88-key piano range:
+// A0 = MIDI 21
+// C8 = MIDI 108
+//
+// A4 = 440 Hz
+//
 
 double noteFrequency(const std::string& note)
 {
-    static const std::map<std::string, double> frequencies =
+    if (note.size() < 2)
+        return 0.0;
+
+    // Note letter
+    char letter = note[0];
+
+    int semitoneFromC = -1;
+
+    switch (letter)
     {
-        {"C2",65.41},{"C#2",69.30},{"D2",73.42},{"D#2",77.78},
-        {"E2",82.41},{"F2",87.31},{"F#2",92.50},{"G2",98.00},
-        {"G#2",103.83},{"A2",110.00},{"A#2",116.54},{"B2",123.47},
+        case 'C': semitoneFromC = 0;  break;
+        case 'D': semitoneFromC = 2;  break;
+        case 'E': semitoneFromC = 4;  break;
+        case 'F': semitoneFromC = 5;  break;
+        case 'G': semitoneFromC = 7;  break;
+        case 'A': semitoneFromC = 9;  break;
+        case 'B': semitoneFromC = 11; break;
 
-        {"C3",130.81},{"C#3",138.59},{"D3",146.83},{"D#3",155.56},
-        {"E3",164.81},{"F3",174.61},{"F#3",185.00},{"G3",196.00},
-        {"G#3",207.65},{"A3",220.00},{"A#3",233.08},{"B3",246.94},
+        default:
+            return 0.0;
+    }
 
-        {"C4",261.63},{"C#4",277.18},{"D4",293.66},{"D#4",311.13},
-        {"E4",329.63},{"F4",349.23},{"F#4",369.99},{"G4",392.00},
-        {"G#4",415.30},{"A4",440.00},{"A#4",466.16},{"B4",493.88},
+    size_t position = 1;
 
-        {"C5",523.25},{"C#5",554.37},{"D5",587.33},{"D#5",622.25},
-        {"E5",659.25},{"F5",698.46},{"F#5",739.99},{"G5",783.99},
-        {"G#5",830.61},{"A5",880.00},{"A#5",932.33},{"B5",987.77},
+    // Sharp
+    if (position < note.size() && note[position] == '#')
+    {
+        semitoneFromC++;
+        position++;
+    }
 
-        {"C6",1046.50},{"C#6",1108.73},{"D6",1174.66},{"D#6",1244.51},
-        {"E6",1318.51},{"F6",1396.91},{"F#6",1479.98},{"G6",1567.98},
-        {"G#6",1661.22},{"A6",1760.00},{"A#6",1864.66},{"B6",1975.53}
-    };
+    // Octave
+    if (position >= note.size())
+        return 0.0;
 
-    auto found = frequencies.find(note);
+    int octave = 0;
 
-    if (found != frequencies.end())
-        return found->second;
+    try
+    {
+        octave = std::stoi(
+            note.substr(position)
+        );
+    }
+    catch (...)
+    {
+        return 0.0;
+    }
 
-    return 0.0;
+    // Handle C# -> wrap into next octave
+    if (semitoneFromC >= 12)
+    {
+        semitoneFromC -= 12;
+        octave++;
+    }
+
+    // MIDI note number
+    int midiNote =
+        (octave + 1) * 12 +
+        semitoneFromC;
+
+    // Standard 88-key piano is MIDI 21 through 108
+    if (midiNote < 21 || midiNote > 108)
+        return 0.0;
+
+    // A4 = MIDI 69 = 440 Hz
+    return
+        440.0 *
+        std::pow(
+            2.0,
+            (midiNote - 69) / 12.0
+        );
 }
-
 // ============================================================
-// NOTE LIST
+// NOTE LIST - FULL 88-KEY PIANO
 // ============================================================
+//
+// A standard piano has 88 keys:
+//
+// A0
+// A#0
+// B0
+// C1 ... B7
+// C8
+//
+// Total = 88 notes
+//
 
 std::vector<std::string> GetNotes()
 {
@@ -225,20 +284,29 @@ std::vector<std::string> GetNotes()
         "F#","G","G#","A","A#","B"
     };
 
-    for (int octave = 2; octave <= 6; ++octave)
+    // MIDI 21 = A0
+    // MIDI 108 = C8
+
+    for (int midi = 21;
+         midi <= 108;
+         ++midi)
     {
-        for (const char* name : noteNames)
-        {
-            notes.push_back(
-                std::string(name) +
-                std::to_string(octave)
-            );
-        }
+        int octave =
+            (midi / 12) - 1;
+
+        int noteIndex =
+            midi % 12;
+
+        notes.push_back(
+            std::string(
+                noteNames[noteIndex]
+            ) +
+            std::to_string(octave)
+        );
     }
 
     return notes;
 }
-
 // ============================================================
 // INSTRUMENT LIST
 // ============================================================
